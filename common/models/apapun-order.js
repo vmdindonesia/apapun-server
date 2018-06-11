110db973a2bb4b6855726f9241e5e975cb4cce10
DESKTOP-6U8JP9R\Ricky <rickyhermawan03@gmail.com>
2018-06-12 03:10:41 +0700

    create order

--- common/models/apapun-order.js
+++ common/models/apapun-order.js
@@ -1,5 +1,54 @@
 'use strict';
 
-module.exports = function(Apapunorder) {
+module.exports = function (Apapunorder) {
+    let request = require("request");
+    let app = require("../../server/server");
 
+    Apapunorder.remoteMethod(
+        'CreateOrder', {
+            accepts: [{
+                arg: 'params',
+                type: 'ApapunOrder',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'CreateOrder', type: 'ApapunOrder', root: true
+            },
+            http: {
+                path: '/CreateOrder',
+                verb: 'post'
+            },
+            description: [
+                'This instance for User Authentication user APAPUN.COM',
+            ]
+        });
+    Apapunorder.CreateOrder = function (params, options, cb, next) {
+        console.log(params, 'Params');
+        Apapunorder.create(params, function (error, token) {
+            // console.log(token);
+            if (error) {
+                cb(error);
+                console.log(error.statusCode, 'Errornya');
+            } else {
+                console.log(token, 'TOKEN');
+                let materialModel = app.models.ApapunOrderMaterial;
+                for (var i = 0; i < params.idMaterial.length; i++) {
+                    console.log(token.id, 'ID ORDER');
+                    materialModel.create({
+                        idMaterial: params.idMaterial[i],
+                        idOrder: token.id
+                    }, function (err, data) {
+                        if (err) {
+                            console.log(err)
+                        }
+                    });
+                }
+            }
+        });
+    };
 };
