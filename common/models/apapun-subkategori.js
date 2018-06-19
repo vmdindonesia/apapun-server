cb9f3056797f5b3c4b728a6ba7f28002ada7746c
DESKTOP-6U8JP9R\Ricky <rickyhermawan03@gmail.com>
2018-06-20 04:27:14 +0700

    penambahan model kategori + subkategori

---
+++ common/models/apapun-subkategori.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = function(ApapunSubkategori) {
+
+    ApapunSubkategori.remoteMethod(
+        'getSubkategori', {
+            accepts: {
+                arg: 'data',
+                type: 'Object',
+                http: { source: 'body' }
+            },
+            returns: {
+                type: 'array', root: true
+            },
+            http: {
+                path: '/getSubkategori',
+                verb: 'post'
+            }
+        });
+
+    ApapunSubkategori.getSubkategori = function (params, cb) {
+        console.log(params, 'Params')
+
+        ApapunSubkategori.find({
+            where:
+                { kategoriId: params.id }
+        }, function (err, result) {
+            if (result) {
+                cb(err, result);
+            } else {
+                cb(err);
+            }
+        })
+    };
+};
