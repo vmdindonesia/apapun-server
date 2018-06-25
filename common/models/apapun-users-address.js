8b3da47bbd2be71f8e5514af18c43085b9da07c6
DESKTOP-6U8JP9R\Ricky <rickyhermawan03@gmail.com>
2018-06-25 23:01:11 +0700

    create useraddress

--- common/models/apapun-users-address.js
+++ common/models/apapun-users-address.js
@@ -2,4 +2,35 @@
 
 module.exports = function(Apapunusersaddress) {
 
+    Apapunusersaddress.remoteMethod(
+        'getUserAddress', {
+            accepts: {
+                arg: 'data',
+                type: 'Object',
+                http: { source: 'body' }
+            },
+            returns: {
+                type: 'array', root: true
+            },
+            http: {
+                path: '/getUserAddress',
+                verb: 'get'
+            }
+        });
+
+    Apapunusersaddress.getUserAddress = function (params, cb) {
+        console.log(params, 'Params')
+
+        Apapunusersaddress.find({
+            where:
+                { provinceId: params.username }
+        }, function (err, result) {
+            if (result) {
+                cb(err, result);
+            } else {
+                cb(err);
+            }
+        })
+    };
+
 };
