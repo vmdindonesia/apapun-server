5507c2cc2fa1226cdca5f438d8ed289f37e379cf
DESKTOP-6U8JP9R\Ricky <rickyhermawan03@gmail.com>
2018-06-29 04:24:23 +0700

    update flow order

--- common/models/apapun-bet.js
+++ common/models/apapun-bet.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(Apapunbet) {
+module.exports = function (Apapunbet) {
     let request = require("request");
     let app = require("../../server/server");
 
@@ -27,66 +27,114 @@ module.exports = function(Apapunbet) {
                 'This instance for User Authentication user APAPUN.COM',
             ]
         });
-    
-        Apapunbet.CreateBet = function (params, options, cb) {
-            console.log(params, 'Params');
-            Apapunbet.create({
-                betId: params.betId,
-                orderId: params.orderId,
-                crafterId: params.crafterId,
-                price: params.price,
-                status: 'pending',
-                description: params.description,
-                createdAt: params.createdAt
-            }, function (error, token) {
-                console.log(token);
-                if (error) {
-                    cb(error);
-                    console.log(error.statusCode, 'Errornya');
-                } else {
-                    cb(error, token);
-                }
-            });
-        };
+
+    Apapunbet.CreateBet = function (params, options, cb) {
+        console.log(params, 'Params');
+        Apapunbet.create({
+            betId: params.betId,
+            orderId: params.orderId,
+            crafterId: params.crafterId,
+            price: params.price,
+            status: 'pending',
+            description: params.description,
+            createdAt: params.createdAt
+        }, function (error, token) {
+            console.log(token);
+            if (error) {
+                cb(error);
+                console.log(error.statusCode, 'Errornya');
+            } else {
+                cb(error, token);
+            }
+        });
+    };
 
     Apapunbet.remoteMethod(
-            'EditBet', {
-                accepts: [{
-                    arg: 'params',
-                    type: 'ApapunBet',
-                    required: true,
-                    http: { source: 'body' }
-                }, {
-                    arg: "options",
-                    type: "object",
-                    http: "optionsFromRequest"
-                }],
-                returns: {
-                    arg: 'EditBet', type: 'ApapunBet', root: true
-                },
-                http: {
-                    path: '/EditBet',
-                    verb: 'post'
-                },
-                description: [
-                    'This instance for User Authentication user APAPUN.COM',
-                ]
+        'EditBet', {
+            accepts: [{
+                arg: 'params',
+                type: 'ApapunBet',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'EditBet', type: 'ApapunBet', root: true
+            },
+            http: {
+                path: '/EditBet',
+                verb: 'post'
+            },
+            description: [
+                'This instance for User Authentication user APAPUN.COM',
+            ]
         });
 
-        Apapunbet.EditBet = function (params, options, cb) {
-                    Apapunbet.updateAll(
-                        { crafterId: params.crafterId },
-                        {
-                            status: params.status
-                        },
-                        function (error, token) {
-                            console.log(token);
-                            if (error) {
-                                cb(error);
-                                console.log(error.statusCode, 'Errornya');
-                            } else {
-                                cb(error, token);
-                            }
-                        });
-                }
+    Apapunbet.EditBet = function (params, options, cb) {
+        if (params.status === 'approve') {
+            Apapunbet.updateAll(
+                { crafterId: params.crafterId },
+                {
+                    status: params.status
+                },
+                function (error, token) {
+                    console.log(token);
+                    if (error) {
+                        cb(error);
+                        console.log(error.statusCode, 'Errornya');
+                    } else {
+                        // cb(error, token);
+                        Apapunbet.updateAll(
+                            { orderId: params.orderId, status: 'pending' },
+                            {
+                                status: 'cancel'
+                            },
+                            function (error, token) {
+                                console.log(token);
+                                if (error) {
+                                    cb(error);
+                                    console.log(error.statusCode, 'Errornya');
+                                } else {
+                                    // cb(error, token);
+                                    let createLogModel = app.models.ApapunOrderLog;
+                                    createLogModel.create({
+                                        description: params.description,
+                                        orderId: params.orderId,
+                                        status: "2"
+                                    }, function (error, token) {
+                                        console.log(token);
+                                        if (error) {        
+                                            cb(error);
+                                            console.log(error.statusCode, 'Errornya');
+                                        } else {
+                                            cb(error, token);
+                                        }
+                                    });
+
+
+
+                                }
+                            });
+                    }
+                });
+        } else {
+            Apapunbet.updateAll(
+                { crafterId: params.crafterId },
+                {
+                    status: params.status
+                },
+                function (error, token) {
+                    console.log(token);
+                    if (error) {
+                        cb(error);
+                        console.log(error.statusCode, 'Errornya');
+                    } else {
+                        cb(error, token);
+                    }
+                });
+        }
+    }
 };
--- common/models/apapun-order-log.js
+++ common/models/apapun-order-log.js
@@ -1,5 +1,55 @@
 'use strict';
 
-module.exports = function(Apapunorderlog) {
+module.exports = function (Apapunorderlog) {
 
+    let request = require("request");
+    let app = require("../../server/server");
+
+    Apapunorderlog.remoteMethod(
+        'CreateStatusLog', {
+            accepts: [{
+                arg: 'params',
+                type: 'ApapunOrderLog',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'CreateStatusLog', type: 'ApapunOrderLog', root: true
+            },
+            http: {
+                path: '/CreateStatusLog',
+                verb: 'post'
+            },
+            description: [
+                'This instance for User Authentication user APAPUN.COM',
+            ]
+        });
+
+    Apapunorderlog.CreateStatusLog = function (params, options, cb) {
+        console.log(params, 'Params');
+        Apapunorderlog.create({
+            description: params.description,
+            orderId: params.orderId,
+            status: params.status
+        }, function (error, token) {
+            console.log(token);
+            if (error) {
+                cb(error);
+                console.log(error.statusCode, 'Errornya');
+            } else {
+                cb(error, token);
+                let orderUpdateFinish = app.models.ApapunOrder;
+                orderUpdateFinish.updateAll(
+                    { orderId: params.orderId },
+                    { status: 'finish' }
+                ), function (error, data) {
+                    cb(error, data);
+                }
+            }
+        });
+    }
 };
