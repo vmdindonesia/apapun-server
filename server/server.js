adda935434a549827d3ebe2c6781a32d603a7f3d
DESKTOP-6U8JP9R\Ricky <rickyhermawan03@gmail.com>
2018-07-13 03:16:10 +0700

    chat model

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 'use strict';
 
+
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
+var consolidate = require('consolidate');
 var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
-  return app.listen(function() {
-    app.emit('started');
+  var server = app.listen(function() {
+    app.emit('started', server);
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
@@ -16,7 +17,9 @@ app.start = function() {
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
+  return server;
 };
+
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -24,6 +27,15 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
+  if (require.main === module){
     app.start();
+  }
+    
 });
+
+// define the templating engine
+app.engine('html', consolidate.pug);
+app.set('view engine', 'pug');
+
+// set the folder where templates can be found    
+app.set('views', __dirname + '/views');
