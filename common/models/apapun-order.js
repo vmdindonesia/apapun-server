1a0a2b9af5fea5f56eac27d780726636887edb41
DESKTOP-R31E4IA\Chucksarmy <fashahd@gmail.com>
2018-07-17 01:23:02 +0700

    bulk insert create order

--- common/models/apapun-order.js
+++ common/models/apapun-order.js
@@ -41,18 +41,20 @@ module.exports = function (Apapunorder) {
                 const ai = data.length + 1;
                 var dataOrder = {
                     orderId: 'ORDER-' + ai,
-                    idUser: params.userId,
+                    idUser: params.idUser,
                     nameProduct: params.nameProduct,
-                    addressIdDelivery: params.addressDelivery,
-                    noteDelivery: params.catatanTambahan,
-                    quantityProduct: params.numberPcs,
+                    addressIdDelivery: params.addressIdDelivery,
+                    noteDelivery: params.noteDelivery,
+                    quantityProduct: params.quantityProduct,
                     unitQuantity: params.unitQuantity,
-                    serviceProduct: params.serveDelivery,
-                    status: 'Active',
-                    unitCategoryProduct: params.categoryProduct,
-                    unitSubCategoryProduct: params.subCategoryProduct,
+                    serviceProduct: params.serviceProduct,
+                    statusOrder: 'Active',
+                    unitCategoryProduct: params.unitCategoryProduct,
+                    unitSubCategoryProduct: params.unitSubCategoryProduct,
                     typeOrder: 'Custom Order',
-                    dataImages: params.nameFileImages
+                    deliveryProvider:params.deliveryProvider,
+                    deliveryProviderType:params.deliveryProviderType,
+                    dataImages: params.images
                 };
                 console.log(dataOrder, 'Data Order');
                 Apapunorder.create(dataOrder, function (error, resultOrder) {
@@ -74,20 +76,24 @@ module.exports = function (Apapunorder) {
                                 console.log(error.statusCode, 'Errornya');
                             } else {
                                 console.log(resultOrderLog, 'Result Order Log');
-                                cb(err, resultOrderLog);
-                                for (var i = 0; i < params.nameFileImages.length; i++) {
-                                    imagesModel.create({
-                                        name: params.nameFileImages[i],
-                                        idOrder: resultOrder.orderId,
-                                        type: 'Custom Order'
-                                    }, function (err, resultImage) {
-                                        if (err) {
-                                            cb(err)
-                                        } else {
-                                            console.log(resultImage, 'Result Images')
-                                        }
-                                    });
+                                var imagePOST = [];
+                                for (var i = 0; i < dataOrder.dataImages.length; i++) {
+                                    imagePOST[i] = {
+                                        'name' : dataOrder.dataImages[i],
+                                        'idOrder' : dataOrder.orderId,
+                                        'type' : 'Custom Order'
+                                    };
                                 }
+
+                                console.log(imagePOST, 'XXX');
+                                imagesModel.create(imagePOST, function (err, resultImage) {
+                                    if (err) {
+                                        cb(err)
+                                    } else {
+                                        console.log(resultImage, 'Result Images');
+                                        cb(err, resultImage);
+                                    }
+                                });
 
                                 // console.log(params.idMaterial);
                                 // for (var i = 0; i < params.idMaterial.length; i++) {
@@ -125,7 +131,7 @@ module.exports = function (Apapunorder) {
             }
         });
 
-        Apapunorder.getHistoryOrder = function (params, cb) {
+    Apapunorder.getHistoryOrder = function (params, cb) {
         console.log(params, 'Params')
 
         Apapunorder.find({
@@ -187,9 +193,9 @@ module.exports = function (Apapunorder) {
                                     cb(err);
                                 } else {
                                     apresiasimodel.create({
-                                        orderId : params.order_id,
-                                        price : params.price,
-                                        userId : params.username
+                                        orderId: params.order_id,
+                                        price: params.price,
+                                        userId: params.username
                                     }, function (error, token) {
                                         console.log(token);
                                         if (error) {
@@ -231,60 +237,60 @@ module.exports = function (Apapunorder) {
                 'This instance for User Authentication user APAPUN.COM',
             ]
         });
-        Apapunorder.deliveryToCustomer = function (params, options, cb) {
-            Apapunorder.findById(params.betId, function (err, data) {
-                if (err) {
-                    cb(err);
-                } else {
-                    // cb(err, data);
-    
-                    console.log(params.resiNumber, 'RESI')
-                    Apapunorder.updateAll(
-                        { orderId: params.orderId },
-                        {
-                            statusOrder: 'delivered'
-                        },
-                        function (error, token) {
-                            console.log(token);
-                            if (error) {
-                                cb(error);
-                            } else {
-                                let betModel = app.models.ApapunBet;
-                            
-                                betModel.updateAll(
-                                    { betId: params.betId },
-                                    {
-                                        status: 'delivered'
-                                    },
-                                    function (error, token) {
-                                        console.log(token);
-                                        if (error) {
-                                            cb(error);
-                                        } else {
-                                            let createLogModel = app.models.ApapunOrderLog;
-                                            createLogModel.create({
-                                                description: params.resiNumber,
-                                                orderId: params.orderId,
-                                                status: '4'
-                                            }, function (error, resultOrderLog) {
-                                                if (error) {
-                                                    cb(error);
-                                                    console.log(error.statusCode, 'Errornya');
-                                                } else {    
-                                                    console.log(params, 'Result Order Log');
-                                                    cb(err, resultOrderLog);
-                                                }
-                                            });
-                                        }
-                                    });
-                                
-                            }
-                        });
-                }
-            });
-        };
+    Apapunorder.deliveryToCustomer = function (params, options, cb) {
+        Apapunorder.findById(params.betId, function (err, data) {
+            if (err) {
+                cb(err);
+            } else {
+                // cb(err, data);
+
+                console.log(params.resiNumber, 'RESI')
+                Apapunorder.updateAll(
+                    { orderId: params.orderId },
+                    {
+                        statusOrder: 'delivered'
+                    },
+                    function (error, token) {
+                        console.log(token);
+                        if (error) {
+                            cb(error);
+                        } else {
+                            let betModel = app.models.ApapunBet;
+
+                            betModel.updateAll(
+                                { betId: params.betId },
+                                {
+                                    status: 'delivered'
+                                },
+                                function (error, token) {
+                                    console.log(token);
+                                    if (error) {
+                                        cb(error);
+                                    } else {
+                                        let createLogModel = app.models.ApapunOrderLog;
+                                        createLogModel.create({
+                                            description: params.resiNumber,
+                                            orderId: params.orderId,
+                                            status: '4'
+                                        }, function (error, resultOrderLog) {
+                                            if (error) {
+                                                cb(error);
+                                                console.log(error.statusCode, 'Errornya');
+                                            } else {
+                                                console.log(params, 'Result Order Log');
+                                                cb(err, resultOrderLog);
+                                            }
+                                        });
+                                    }
+                                });
+
+                        }
+                    });
+            }
+        });
+    };
 
 
 
-        //kkkk
+    //kkkk
 };
