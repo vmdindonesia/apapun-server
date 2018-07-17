31cd5e90a1e9a96bf1f0b396a0d9b7db195b7602
DESKTOP-R31E4IA\Chucksarmy <fashahd@gmail.com>
2018-07-17 15:49:49 +0700

    add create material and sub material

--- common/models/apapun-material.js
+++ common/models/apapun-material.js
@@ -1,5 +1,43 @@
 'use strict';
 
 module.exports = function(Apapunmaterial) {
-
+    let request = require("request");
+    let app = require("../../server/server");
+    Apapunmaterial.remoteMethod(
+        'CreateNewMaterial', {
+            accepts: [{
+                arg: 'params',
+                type: 'ApapunMaterial',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'CreateNewMaterial', type: 'ApapunMaterial', root: true
+            },
+            http: {
+                path: '/CreateNewMaterial',
+                verb: 'post'
+            },
+            description: [
+                'This instance for Create New Material',
+            ]
+        });
+    Apapunmaterial.CreateNewMaterial = function (params, options, cb) {
+        console.log(params, 'Params');
+        Apapunmaterial.create({
+            materialName : params.materialName,
+            createdUserId : params.createdUserId
+        }, function (error, token) {
+            console.log(token);
+            if (error) {
+                console.log(error, 'Errornya');
+            } else {
+                cb(error, token);
+            }
+        });
+    };
 };
--- common/models/apapun-submaterial.js
+++ common/models/apapun-submaterial.js
@@ -1,5 +1,44 @@
 'use strict';
 
 module.exports = function(Apapunsubmaterial) {
-
+    let request = require("request");
+    let app = require("../../server/server");
+    Apapunsubmaterial.remoteMethod(
+        'CreateSubMaterial', {
+            accepts: [{
+                arg: 'params',
+                type: 'ApapunSubmaterial',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'CreateSubMaterial', type: 'ApapunSubmaterial', root: true
+            },
+            http: {
+                path: '/CreateSubMaterial',
+                verb: 'post'
+            },
+            description: [
+                'This instance for Create Sub Material',
+            ]
+        });
+    Apapunsubmaterial.CreateSubMaterial = function (params, options, cb) {
+        console.log(params, 'Params');
+        Apapunsubmaterial.create({
+            materialName : params.materialName,
+            createdUserId : params.createdUserId,
+            materialId : params.materialId
+        }, function (error, token) {
+            console.log(token);
+            if (error) {
+                console.log(error, 'Errornya');
+            } else {
+                cb(error, token);
+            }
+        });
+    };
 };
