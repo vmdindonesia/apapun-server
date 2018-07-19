02a0a3804f440bb105f3ebda5a6c4342990100f6
Fashah DArullah <fashahd@gmail.com>
2018-07-20 00:50:17 +0700

    hjhjhjh

--- common/models/apapun-storage.js
+++ common/models/apapun-storage.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(Apapunstorage) {
-
+    Apapunstorage.upload = function(req,res,cb) {
+           Apapunstorage.createContainer({name:user.name},function(err,c) {
+            Apapunstorage.upload(req,res,{container: 'images'},cb)
+           });
+        }
+        Apapunstorage.remoteMethod (
+           'upload',
+           {
+            http: {path: '/upload', verb: 'post'},
+            accepts: [
+               {arg: 'req', type: 'object', 'http': {source: 'req'}},
+               {arg: 'res', type: 'object', 'http': {source: 'res'}}
+            ],
+            returns: {arg: 'status', type: 'string'}
+           }
+        );
 };
