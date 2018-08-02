347ba53dd0777e29f933ae0a3b22958b025a0055
DESKTOP-SVFRPOF\Chucksarmy <fashahd@gmail.com>
2018-08-03 02:17:24 +0700

    add autocomplete search

--- common/models/apapun-districts.js
+++ common/models/apapun-districts.js
@@ -33,4 +33,41 @@ module.exports = function(Apapundistricts) {
         })
     };
 
+    Apapundistricts.remoteMethod(
+        'getDistrictAuto', {
+            accepts: [{
+                arg: 'params',
+                type: 'object',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'getDistrictAuto', type: 'object', root: true
+            },
+            http: {
+                path: '/getDistrictAuto',
+                verb: 'post'
+            },
+            description: [
+                'This instance for User Authentication user APAPUN.COM',
+            ]
+        });
+    Apapundistricts.getDistrictAuto = function (params, options, cb, next) {
+        console.log(params, 'Params Nya');
+        var ds = Apapundistricts.dataSource;
+        const sql = " SELECT * FROM `apapun_districts` WHERE `name` like '%"+params.keyword+"%'";
+        ds.connector.execute(sql, function (err, result) {
+            if (err) {
+                cb(err);
+                return;
+            }else{
+                cb(null,result);
+            }
+        });
+    };
+
 };
--- common/models/apapun-regencies.js
+++ common/models/apapun-regencies.js
@@ -33,4 +33,41 @@ module.exports = function (Apapunregencies) {
         })
     };
 
+    Apapunregencies.remoteMethod(
+        'getRegenciesAuto', {
+            accepts: [{
+                arg: 'params',
+                type: 'object',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'getRegenciesAuto', type: 'object', root: true
+            },
+            http: {
+                path: '/getRegenciesAuto',
+                verb: 'post'
+            },
+            description: [
+                'This instance for User Authentication user APAPUN.COM',
+            ]
+        });
+    Apapunregencies.getRegenciesAuto = function (params, options, cb, next) {
+        console.log(params, 'Params Nya');
+        var ds = Apapunregencies.dataSource;
+        const sql = " SELECT * FROM `apapun_regencies` WHERE `name` like '%"+params.keyword+"%'";
+        ds.connector.execute(sql, function (err, result) {
+            if (err) {
+                cb(err);
+                return;
+            }else{
+                cb(null,result);
+            }
+        });
+    };
+
 };
