a24e3fe202f99634b2e1ef3eb0582a30d4d47fb3
DESKTOP-SVFRPOF\Chucksarmy <fashahd@gmail.com>
2018-08-08 03:18:54 +0700

    add autocomplete material search

--- common/models/apapun-material.js
+++ common/models/apapun-material.js
@@ -3,6 +3,44 @@
 module.exports = function(Apapunmaterial) {
     let request = require("request");
     let app = require("../../server/server");
+
+    
+    Apapunmaterial.remoteMethod(
+        'GetMaterialAuto', {
+            accepts: [{
+                arg: 'params',
+                type: 'object',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'GetMaterialAuto', type: 'object', root: true
+            },
+            http: {
+                path: '/GetMaterialAuto',
+                verb: 'post'
+            },
+            description: [
+                'This instance for Autocomplete Material',
+            ]
+        });
+    Apapunmaterial.GetMaterialAuto = function (params, options, cb) {
+        console.log(params, 'Params');
+        var ds = Apapunmaterial.dataSource;
+        const sqlRow = " SELECT * FROM `apapun_material` WHERE material_name like '%"+params.keyword+"%' ORDER BY material_name asc limit 6";
+        ds.connector.query(sqlRow, function (err, data) {
+            if (err) {
+                console.log(err, 'ERROR QUERY USER ID');
+            } else {
+                cb(err,data);
+            }
+        });
+    };
+
     Apapunmaterial.remoteMethod(
         'CreateNewMaterial', {
             accepts: [{
