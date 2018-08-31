0a1108b3311df8aad0e9e69279959c806683d85e
DESKTOP-SVFRPOF\Chucksarmy <fashahd@gmail.com>
2018-08-31 11:05:22 +0700

    add crafter note

--- common/models/apapun-crafter-note.js
+++ common/models/apapun-crafter-note.js
@@ -125,4 +125,47 @@ module.exports = function(Apapuncrafternote) {
         });
     };
 
+    Apapuncrafternote.remoteMethod(
+        'deleteCrafterNote', {
+            accepts: [{
+                arg: 'params',
+                type: 'ApapunCrafterNote',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'deleteCrafterNote', type: 'ApapunCrafterNote', root: true
+            },
+            http: {
+                path: '/deleteCrafterNote',
+                verb: 'post'
+            },
+            description: [
+                'This instance for User Authentication user APAPUN.COM',
+            ]
+        });
+    Apapuncrafternote.deleteCrafterNote = function (params, options, cb) {
+        Apapuncrafternote.find({id:params.id, crafterId:params.crafterId}, function (err, data) {
+            if (err) {
+                cb(err);
+            } else {
+                Apapuncrafternote.destroyAll(
+                    { id:params.id,crafterId: params.crafterId },
+                    function (error, token) {
+                        console.log(token);
+                        if (error) {
+                            cb(error);
+                            console.log(error.statusCode, 'Errornya');
+                        } else {
+                            cb(error, token);
+                        }
+                    });
+            }
+        });
+    };
+
 };
